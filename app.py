295fe90b846105335d205ebc58c1feadbfc6b12c
SobolevDA <shulbtz@gmail.com>
2019-05-20 13:26:21 +0300

    add request.get for templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from query_select import service_customer
 
 app = Flask(__name__)
@@ -7,6 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html', service=service_customer)
+
+
+@app.route('/count/', methods=['POST'])
+def count():
+    begin_date = request.form.get('begin_date')
+    end_date = request.form.get('end_date')
+    print(end_date)
+    print(begin_date)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
