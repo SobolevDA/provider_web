f0e0a51dc494dfeb54d970b45ea576f8b9170958
SobolevDA <shulbtz@gmail.com>
2019-05-21 13:21:54 +0300

    select all customer for ethernet

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from query_select import service_customer, count
+from query_select import service_customer, count_dis
 from query_date import date_time
 
 app = Flask(__name__)
@@ -10,21 +10,26 @@ def hello():
     return render_template('index.html', service=service_customer)
 
 
-@app.route('/count/', methods=['POST'])
+@app.route('/ethernet/', methods=['POST'])
 def count():
-    begin_date = request.form.get('begin_date')
-    begin_date = date_time(begin_date)
-
-    end_date = request.form.get('end_date')
-    end_date = date_time(end_date)
-
-    service_id = request.form.get('id_name.id')
-    print(service_id)
-
-    print(end_date)
+    begin = request.form.get('begin')
+    begin_date = date_time(begin)
     print(begin_date)
 
-    return render_template('index.html')
+    end = request.form.get('end')
+    end_date = date_time(end)
+    print(end_date)
+
+    id_service = request.form.get('services')
+    print(id_service)
+    
+    discount = count_dis(begin_date, end_date, id_service)
+    print(discount)
+
+    return render_template('inform.html', 
+                           discount=discount,
+                           begin=begin, end=end,
+                           service=service_customer)
 
 
 if __name__ == '__main__':
--- query_select.py
+++ query_select.py
@@ -4,17 +4,18 @@ from models_db import LinkCustomerService as LCS
 
 database.connect()
 
+
 service_customer = Service.select(Service.id, Service.name).where(Service.id << (97, 59, 58, 47, 50, 48, 46, 153, 49, 137))
 
 
-def count(begin_date, end_date, service_id):
-    count = []
-    count_discount = (DIS.select(DIS.link_customer_service).where((DIS.discount_time > begin_date) 
-                  & (DIS.discount_time < end_date) 
-                 & (DIS.link_customer_service << (LCS.select(LCS.id).where((LCS.service == service_customer) 
-                  & (LCS.link_time < end_date) & (LCS.unlink_time >> None) 
-                   & (LCS.is_deleted >> None))))))
+def count_dis(begin, end, id):
+    dis = []
+    count_discount = (DIS.select(DIS.link_customer_service).where((DIS.discount_time > begin) 
+                      & (DIS.discount_time < end) 
+                      & (DIS.link_customer_service << (LCS.select(LCS.id).where((LCS.service == id) 
+                         & (LCS.link_time < end) & (LCS.unlink_time >> None) 
+                       & (LCS.is_deleted >> None))))).distinct())
     
     for i in count_discount:
-        count.append(i)
-    return len(count)
+        dis.append(i)
+    return len(dis)
